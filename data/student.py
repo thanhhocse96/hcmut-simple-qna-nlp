238984173b846690943cef8f9480de13f28b1cd8
Thanh Ho Quang <thanh.hoquangcse96@gmail.com>
2020-12-28 13:40:38 +0700

    Add data type Student to read input

---
+++ data/student.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, full_name, id, subject, room, day, start_time, class_length):
+        self.full_name = full_name
+        self.id = id
+        self. subject = subject
+        self.room = room
+        self.day = day
+        self.start_time = start_time
+        self.class_length = class_length
+
+    def 
+
